dc27bd8f4bd149509cc1ba567735979a21921a54
CouCave <phankhang1945@gmail.com>
2026-01-19 19:22:33 +0700

    Hoàn thành Lab 4

---
+++ slide4/src/components/Demo.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    data() {
+        return {
+            message: 'Xin chào VueJS',
+            number: 10,
+            content: 'Xin chào các bạn',
+            className: 'text-red',
+            styleData: 'font-size: 5rem'
+        }
+    },
+    methods: {
+        sayHi() {
+            return 'Đây là phương thức'
+        },
+        changeContent() {
+            this.message = 'Đây là nội dung mới'
+        },
+        change(){
+            this.content = 'Đã đổi thành nội dung mới'
+            this.className = 'text-blue'
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h2 v-bind:class="className" v-bind:style="styleData" v-html="content"></h2>
+        <h6 class="text-red" style="font-size: 5rem">{{ content }}</h6>
+        <h1>Hiển thị nội dung</h1>
+        <h2>{{ message }}</h2>
+        <h2 v-html="number + 5"></h2>
+        <h4>{{ sayHi() }}</h4>
+        <button @click="change()">Change</button>
+    </div>
+</template>
+
+<style>
+    .text-red{
+        color:red
+    }
+    .text-blue{
+        color:blue
+    }
+</style>
---
+++ slide4/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+<h1>Chào mừng đến với VueJS</h1>
+</template>
+
+<style>
+h1{color: red;}
+</style>
+
